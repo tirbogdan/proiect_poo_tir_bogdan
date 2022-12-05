52c8546f632662db1fab74497b2afb82ea4e0c08
BOGDAN-DANIEL TIR <tir.bogdan1905@gmail.com>
2022-12-05 12:30:21 +0200

    Creare login

--- proiect_poo/proiect_poo/Source.cpp
+++ proiect_poo/proiect_poo/Source.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
 using namespace std;
+
+bool autentificare(string parola)
+{
+	ifstream fin("password.txt");
+	string password;
+	fin >> password;
+	if (parola == password) return true;
+	return false;
+}
 
 int main()
 {
-	cout << "Test";
-	cout << "\nTest2";
-	cout << "\nTest3"
+	bool conectat = false;
+	cout << "Bine ai venit la aplicatia de gestiune a biletelor unui teatru\n";
+	cout << "Pentru a continua te rugam sa introduci parola: ";
+	
+	for (int i = 2; i >= 0; i--)
+	{
+		string parola;
+		cin >> parola;
+		if (autentificare(parola))
+		{
+			cout << "Felicitari, acum esti conectat";
+			conectat = true;
+			break;
+		}
+		else 
+		{ 
+			system("CLS");
+			cout << "Imi pare rau, parola este incorecta\n"; 
+			cout << "Mai ai " << i << " incercari\n";
+		}
+		if (conectat == false && i == 0)
+		{
+			system("CLS");
+			cout << "Ai gresit parola de 3 ori deci nu mai ai acces la aplicatie";
+			return 0;
+		}
+		cout << "Pentru a continua te rugam sa introduci parola: ";
+	}
+	system("CLS");
+	cout << "Felicitari acum esti conectat";
+	
 	return 0;
 }
